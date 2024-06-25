f6c36ac34f0c6d1f8f163272e60e0d3cd0669383
User <User@DESKTOP-3GSOFRD>
2024-06-25 15:40:29 +0530

    Commit Additon Program

---
+++ projectsmaven/src/main/java/projectsmaven/Addition.java
@@ -0,0 +1,13 @@
+package projectsmaven;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1000;
+		int b = 2000;
+		int c = a+b;
+		System.out.println("Addition is:" +c);
+	}
+
+}
